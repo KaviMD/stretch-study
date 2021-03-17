083227a5ef240298075b4bbd95656f59ae1915e6
Kavi Dey <kaviasher@gmail.com>
2021-03-16 23:04:32 -0700

    improve ngram data analysis

--- ngram_prediction.py
+++ ngram_prediction.py
@@ -7,9 +7,11 @@ from sklearn.model_selection import KFold
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import seaborn as sns
 
 import simplejson as json
+import math
 
 sns.set()
 # %%
@@ -155,22 +157,29 @@ for task in events_grouped:
 
                 total_accuracy += total_correct / total_checked
                 total_threshold_accuracy += threshold_correct / threshold_checked
-            ngram_results.append([task, n, confidence_threshold, total_accuracy/k, total_threshold_accuracy/k, total_grams, total_above_threshold])
+            ngram_results.append([task, n, confidence_threshold, total_accuracy/k, total_threshold_accuracy/k, total_grams, total_above_threshold, threshold_correct])
 
 # %%
-df = pd.DataFrame(ngram_results, columns=['task_number', 'n', 'confidence_threshold', 'total_accuracy', 'total_threshold_accuracy', 'total_grams', 'total_above_threshold'])
+df = pd.DataFrame(ngram_results, columns=['task_number', 'n', 'confidence_threshold', 'total_accuracy', 'total_threshold_accuracy', 'total_grams', 'total_above_threshold', 'total_threshold_correct'])
+#df['normalized_prediction_accuracy'] = (df['total_threshold_correct'] / df['total_above_threshold']) * df['total_above_threshold']
 df.to_csv('data/ngram_results.csv', index=False)
 df.head(100)
 
 #%%
 df = pd.read_csv("data/ngram_results.csv")
-
+df['total_threshold_incorrect'] = df['total_above_threshold'] - df['total_threshold_correct']
+df['normalized_accuracy'] = df['total_threshold_correct'] / df['total_threshold_incorrect']
+#df['fixed_accuracy'] = (df['total_above_threshold'] * df['total_threshold_accuracy']) / df['total_above_threshold']
+#df['normalized_predicted_accuracy'] = (df['fixed_accuracy'] * df['total_above_threshold'])
+df.to_csv('data/ngram_results.csv', index=False)
 # %%
-fig, big_axes = plt.subplots(nrows=4, ncols=1, figsize=(20, 25))
-plt.subplots_adjust(hspace=0.35)
+r = 4
+c = 4
+fig, big_axes = plt.subplots(nrows=r, ncols=1, figsize=(30, 25))
+plt.subplots_adjust(hspace=0.4)
 
 for row, big_ax in enumerate(big_axes, start=1):
-    big_ax.set_title(f"Task {row}", fontsize=16)
+    big_ax.set_title(f"Task {row}", fontsize=16,  y=1.08)
 
     # Turn off axis lines and ticks of the big subplot 
     # obs alpha is 0 in RGBA string!
@@ -183,14 +192,35 @@ for i in range(0, 4):
     n_confidence_accuracy = filtered_data.pivot(index='n', columns='confidence_threshold', values='total_threshold_accuracy')
     n_confidence_removed = filtered_data.pivot(index='n', columns='confidence_threshold', values='total_above_threshold')
 
-    g = sns.heatmap(n_confidence_accuracy, ax=fig.add_subplot(4,2,i*2+1))
+    log_norm = LogNorm(vmin=n_confidence_removed.min().min(), vmax=n_confidence_removed.max().max())
+    cbar_ticks = [math.pow(10, i) for i in range(math.floor(math.log10(n_confidence_removed.min().min())), 1+math.ceil(math.log10(n_confidence_removed.max().max())))]
+
+
+    g = sns.heatmap(n_confidence_accuracy, ax=fig.add_subplot(r,c,i*c+1))
     g.set_title('N & Confidence Threshold vs. Accuracy')
     g.set_xticklabels(g.get_xticklabels(), rotation=45)
+    g.set_ylabel('Ngram Size')
+    g.set_xlabel('Confidence Threshold')
 
-    h = sns.heatmap(n_confidence_removed, ax=fig.add_subplot(4,2,i*2+2))
+    h = sns.heatmap(n_confidence_removed, ax=fig.add_subplot(r,c,i*c+2), norm=log_norm, cbar_kws={"ticks": cbar_ticks})
     h.set_title('N & Confidence Threshold vs. # of Predictions Made')
     h.set_xticklabels(h.get_xticklabels(), rotation=45)
+    h.set_ylabel('Ngram Size')
+    h.set_xlabel('Confidence Threshold')
+
+    
+    j = sns.scatterplot(data=filtered_data, x='total_threshold_accuracy', y='total_threshold_correct', hue='n', ax=fig.add_subplot(r,c,i*c+3))
+    j.set_title('Accuracy vs. # of Correct Predictions Made')
+    j.set_xlabel('Accuracy')
+    j.set_ylabel('# of Correct Predictions Made')
+
+    
+    k = sns.scatterplot(data=filtered_data, x='total_threshold_accuracy', y='total_threshold_incorrect', hue='n', ax=fig.add_subplot(r,c,i*c+4))
+    k.set_title('Accuracy vs. # of Incorrect Predictions Made')
+    k.set_xlabel('Accuracy')
+    k.set_ylabel('# of Incorrect Predictions Made')
 
 plt.show()
+fig.savefig("data/ngram_results.png")
 
 # %%
